f69a90d744b66420f0739f601c59d5b922fac610
EngMutabazi <engmutabazi2020@gmail.com>
2023-03-18 13:52:12 +0200

    add new function

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
 class Employee:
-    pass
-
-print("heelo")
+    def __init__(self, first_name, last_name, pay):
+        self.first_name= first_name
+        self.last_name= last_name
+        self.pay= pay
+    
+    def fullname(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+emp_1= Employee('John', 'Eduin', 600000)
+print(emp_1.fullname())
