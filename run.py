d05b267127c3c1e829a34723371cb35176408f03
EngMutabazi <engmutabazi2020@gmail.com>
2023-03-18 14:55:37 +0200

    instance and class variables

--- run.py
+++ run.py
@@ -1,10 +1,22 @@
 class Employee:
+    number_of_employee=0
+    raise_amount=1.04
     def __init__(self, first_name, last_name, pay):
         self.first_name= first_name
         self.last_name= last_name
         self.pay= pay
+        self.email= first_name + '.'+ last_name + '@gmail.com'
+        Employee.number_of_employee +=1
     
-    def fullname(self):
-        return '{} {}'.format(self.first_name, self.last_name)
+    def personal_informations(self):
+        return '{} {} {}'.format(self.first_name, self.last_name, self.email)
+    
+    def apply_raise(self):
+        self.pay= int(self.pay * self.raise_amount)
+    
+
 emp_1= Employee('John', 'Eduin', 600000)
-print(emp_1.fullname())
+emp_2= Employee('Jacque', 'Milla', 480000)
+emp_1.raise_amount= 1.05
+
+print(Employee.number_of_employee)
