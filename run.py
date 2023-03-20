9963039a8dd34df951f767cef607eb39d30fa0b0
EngMutabazi <engmutabazi2020@gmail.com>
2023-03-20 15:01:58 +0200

    property method

--- run.py
+++ run.py
@@ -5,33 +5,20 @@ class Employee:
         self.first_name= first_name
         self.last_name= last_name
         self.pay= pay
-        self.email= first_name + '.'+ last_name + '@gmail.com'
         Employee.number_of_employee +=1
-    
-    def personal_informations(self):
-        return '{} {} {}'.format(self.first_name, self.last_name, self.email)
-    
+
+    @property
+    def email(self):
+        return'{}.{}@gmail.com'.format(self.first_name, self.last_name)
+    @property
     def fullname(self):
         return '{} {}'.format(self.first_name, self.last_name)
     
-    def apply_raise(self):
-        self.pay= int(self.pay * self.raise_amount)
-    def __repr__(self): # is used for debugging and loging (recreate object)
-        return "Employee('{}','{}', '{}')".format(self.first_name, self.last_name, self.pay)
-    
-    def __str__(self): # is used as a display to enduser
-        return '{}-{}'.format(self.fullname(),self.email)
-    
-    def __add__(self, other):
-         return self.pay + other.pay
-    
-emp_1= Employee('John', 'Eduin', 600000)
-emp_2= Employee('Jacque', 'Milla', 480000)
+emp_1= Employee('John', 'Smith', 5000)
+
+emp_1.first_name= 'jimy'
+print(emp_1.first_name)
+print(emp_1.email)
+print(emp_1.fullname)
 
 
-
-print(emp_1+emp_2)
-# print(repr(emp_1))
-# print(str(emp_1))
-# print(emp_1.__repr__())
-# print(emp_1.__str__())
