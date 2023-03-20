4738c62fae2a04529fcfb0c44a27dd13e1e9a23f
EngMutabazi <engmutabazi2020@gmail.com>
2023-03-20 13:31:26 +0200

    nheritance

--- run.py
+++ run.py
@@ -11,24 +11,43 @@ class Employee:
     def personal_informations(self):
         return '{} {} {}'.format(self.first_name, self.last_name, self.email)
     
+    def fullname(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+    
     def apply_raise(self):
         self.pay= int(self.pay * self.raise_amount)
     
-    @classmethod  # Here we receive a class as our first argument instead self instance
-    def set_raise_amount(cls, amount):
-        cls.raise_amount= amount
-    @classmethod
-    def from_string(cls, emp_str):
-        firstname, lastname, pay= emp_str.split('-')
-        return cls(firstname, lastname, pay)
-    def is_work_day(day):
-        if day.weekday()==5 or day.weekday()==6:
-            return 'Is not a working day'
-        return 'Is working day'
-    
-emp_1= Employee('John', 'Eduin', 600000)
-emp_2= Employee('Jacque', 'Milla', 480000)
+class Developer(Employee):
+    raise_amount=1.10
+    def __init__(self, first_name, last_name, pay, prog_lang):
+        super().__init__(first_name, last_name, pay)
+        self.prog_lang= prog_lang
 
-import datetime
-my_date= datetime.date(2022,7,11)
-print(Employee.is_work_day(my_date))
+class Manager(Employee):
+    def __init__(self, first_name, last_name, pay, employees= None):
+        super().__init__(first_name, last_name, pay)
+        if employees is None:
+            self.employees= []
+        else:
+            self.employees= employees
+
+    def add_emp(self,emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self,emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print('-->', emp.fullname())     
+
+dev_1= Developer('John', 'Eduin', 600000, 'python')
+dev_2= Developer('Jacque', 'Milla', 480000, 'c++')
+
+mngr_1= Manager('king','smith', 105000,[dev_1])
+print(mngr_1.email)
+mngr_1.add_emp(dev_2)
+mngr_1.remove_emp(dev_1)
+mngr_1.print_emps()
